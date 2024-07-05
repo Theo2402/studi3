ef2a7d3d8a6be6cdd2490cba9f5408232227db28
theodiloreto <tdiloreto24@gmail.com>
2024-07-05 17:25:06 +0200

    Add favicon and manifest, update CSP settings

--- django_jo/settings.py
+++ django_jo/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
        
-        'DIRS': [os.path.join(BASE_DIR, 'frontend')], #'frontend/build'
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')], #'frontend/build'
 
         'APP_DIRS': True,
         'OPTIONS': {
