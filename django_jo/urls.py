535e0b1b6c5433c306d53215f5559092853a87ea
theodiloreto <tdiloreto24@gmail.com>
2024-07-05 19:32:11 +0200

    Add favicon and manifest, update CSP settings

--- django_jo/urls.py
+++ django_jo/urls.py
@@ -38,8 +38,8 @@ urlpatterns = [
     path('api/register/', RegisterUserAPIView.as_view(), name='register'), 
     path('', TemplateView.as_view(template_name='index.html')),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
      
 
 
